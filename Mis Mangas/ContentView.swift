a16c0054b8100680cf79ffa3fb66f9ee260d10f3
Luis David Diaz Ramirez <luis.david.10@hotmail.com>
2024-12-07 23:25:46 +0100

    Initial Commit

---
+++ Mis Mangas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mis Mangas
+//
+//  Created by luis david diaz ramirez on 7/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mis Mangas/Mis_MangasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mis_MangasApp.swift
+//  Mis Mangas
+//
+//  Created by luis david diaz ramirez on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Mis_MangasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
